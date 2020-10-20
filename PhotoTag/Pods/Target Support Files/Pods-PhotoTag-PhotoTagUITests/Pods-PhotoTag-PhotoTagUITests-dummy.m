67f73dc0ddaf2f0a94acd73efe62f8a2f6a0914d
dev-Lena <keunnalee@gmail.com>
2020-10-20 18:26:09 +0900

    chore: add dependency manager
    - CocoaPods
    
    #1

---
+++ PhotoTag/Pods/Target Support Files/Pods-PhotoTag-PhotoTagUITests/Pods-PhotoTag-PhotoTagUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoTag_PhotoTagUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoTag_PhotoTagUITests
+@end
---
+++ PhotoTag/Pods/Target Support Files/Pods-PhotoTagTests/Pods-PhotoTagTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoTagTests : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoTagTests
+@end
