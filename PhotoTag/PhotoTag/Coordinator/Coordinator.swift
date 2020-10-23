995bc66cdad0d31f2c4a42ebe2fa99a16a1c7f61
dev-Lena <keunnalee@gmail.com>
2020-10-23 15:55:13 +0900

    feat: implement Coordinator protocol to Coordinators
    #8

---
+++ PhotoTag/PhotoTag/Coordinator/Coordinator.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2020/10/23.
+//
+
+import UIKit
+
+public protocol Coordinator: class {
+    
+    var childCoordinator: [Coordinator] { get set }
+    
+    init(navigationController: UINavigationController)
+    
+    func start()
+}
