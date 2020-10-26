9dd3e36e9cd092842e89c4d4e4975f5e36d16a91
dev-Lena <keunnalee@gmail.com>
2020-10-26 20:03:04 +0900

    refactor: rename loginContentView to contentView in LoginViewController
    #8

--- PhotoTag/PhotoTag/Login/LoginViewController.swift
+++ PhotoTag/PhotoTag/Login/LoginViewController.swift
@@ -15,44 +15,43 @@ class LoginViewController: UIViewController {
     
     weak var delegate: LoginDelegate?
     
-    private let loginContentView: UIView = {
+    private let contentView: UIView = {
         let view = UIView()
-        view.backgroundColor = .gray
+        view.backgroundColor = .white
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
-    let btnLogin: UIButton = {
-        let btn = UIButton(type: .system)
-        btn.backgroundColor = .blue
-        btn.setTitle("Login", for: .normal)
-        btn.tintColor = .white
-        btn.layer.cornerRadius = 5
-        btn.clipsToBounds = true
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.addTarget(self, action: #selector(navigateToTagCategory), for: .touchUpInside)
-        return btn
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .blue
+        button.setTitle("Sign in with Apple", for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 5
+        button.clipsToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(navigateToTagCategory), for: .touchUpInside)
+        return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
-        loginContentView.addSubview(btnLogin)
-        view.addSubview(loginContentView)
-        loginContentView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        loginContentView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        loginContentView.heightAnchor.constraint(equalToConstant: view.frame.height).isActive = true
-        loginContentView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        contentView.addSubview(loginButton)
+        view.addSubview(contentView)
+        contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        contentView.heightAnchor.constraint(equalToConstant: view.frame.height).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
-        btnLogin.topAnchor.constraint(greaterThanOrEqualTo: self.view.centerYAnchor, constant: 100).isActive = true
-        btnLogin.leadingAnchor.constraint(equalTo: loginContentView.leadingAnchor, constant: 20).isActive = true
-        btnLogin.trailingAnchor.constraint(equalTo: loginContentView.trailingAnchor, constant: -20).isActive = true
-        btnLogin.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        loginButton.topAnchor.constraint(greaterThanOrEqualTo: self.view.centerYAnchor, constant: 100).isActive = true
+        loginButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20).isActive = true
+        loginButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         title = "\(String(describing: self))"
     }
     
     @objc func navigateToTagCategory(_ sender: Any) {
         self.delegate?.navigateToTagCategory()
     }
-    
 }
