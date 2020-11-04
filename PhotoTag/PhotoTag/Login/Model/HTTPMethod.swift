42b3c37d7a4ac06ca40eb4225ad906189f03216f
dev-Lena <keunnalee@gmail.com>
2020-11-04 14:27:47 +0900

    feat: implement Login Manager
    - add related protocol and model
    
    #10

---
+++ PhotoTag/PhotoTag/Login/Model/HTTPMethod.swift
@@ -0,0 +1,29 @@
+//
+//  HTTPMethod.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2020/11/03.
+//
+
+import Foundation
+
+enum HTTPMethod: CustomStringConvertible {
+    
+    case get
+    case put
+    case post
+    case delete
+    
+    var description: String {
+        switch self {
+        case .get:
+            return "GET"
+        case .put:
+            return "PUT"
+        case .post:
+            return "POST"
+        case .delete:
+            return "DELETE"
+        }
+    }
+}
---
+++ PhotoTag/PhotoTag/Login/Model/LoginManager.swift
@@ -0,0 +1,64 @@
+//
+//  LoginManager.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2020/11/01.
+//
+
+import Foundation
+import AuthenticationServices
+import Combine
+
+struct AppleLogin: Codable {
+    let token: String
+}
+
+class LoginManager {
+    
+    // MARK: - properties
+    var authorizationRequests: ASAuthorizationAppleIDRequest {
+        let request = ASAuthorizationAppleIDProvider()
+            .createRequest()
+        request.requestedScopes = [.fullName,
+                                   .email]
+        return request
+    }
+    
+    // MARK: - functions
+    func requestAppleLoginToken(credential: ASAuthorizationAppleIDCredential) {
+        guard let tokenData = credential.identityToken,
+              let token = String(data: tokenData, encoding: .utf8) else { return }
+        // 네트워크 요청
+        UseCase.shared
+            .request(data: AppleLogin(token: token),
+                     endpoint: Endpoint(path: .appleLogin),
+                     method: .post)
+            .receive(subscriber: Subscribers.Sink(receiveCompletion: { [weak self] in
+                guard case let .failure(error) = $0 else { return }
+                debugPrint(error.message)
+                // TODO: - present alertController
+            }, receiveValue: { [weak self] response in
+                self?.checkResponseStatus(statusCode: response.statusCode, token: token)
+            }))
+    }
+    
+    private func checkResponseStatus(statusCode: Int, token: String) {
+        switch statusCode {
+        case 200 ..< 300:
+            try? saveUserInKeychain(token)
+            UserDefaults.standard.set("apple", forKey: "loginType")
+        // TODO: - 화면 
+        default:
+            debugPrint("유효하지 않은 Apple ID 입니다.")
+        }
+    }
+    
+    func saveUserInKeychain(_ userIdentifier: String) {
+        do {
+            try KeychainItem(service: "com.lena.SimLeeTag.PhotoTag", account: "userIdentifier").saveItem(userIdentifier)
+        } catch {
+            debugPrint("Unable to save userIdentifier to keychain.")
+        }
+    }
+    
+}
