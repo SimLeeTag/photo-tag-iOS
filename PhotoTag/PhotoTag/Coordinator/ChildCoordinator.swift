569ac339bf562e71f7ecb63e6414cc95e22d551b
dev-Lena <keunnalee@gmail.com>
2020-11-06 03:07:41 +0900

    feat: add ChildCoordinator class for reduce duplicated code
    #13

---
+++ PhotoTag/PhotoTag/Coordinator/ChildCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  ChildCoordinator.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2020/11/06.
+//
+
+import UIKit
+
+class ChildCoordinator: NSObject, Coordinator {
+    
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+    let appViewControllerFactory = AppViewControllersFactory()
+    
+    required init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        
+    }
+}
