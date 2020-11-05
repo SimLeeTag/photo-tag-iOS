e92abf9ea009964d84ff707b2289d19f10594025
dev-Lena <keunnalee@gmail.com>
2020-11-06 05:54:17 +0900

    feat: create view controller classes and implement basic code
    - PhotoNoteViewController, SelectPhotoViewController, WritePhotoNoteViewController, SelectPeriodViewController
    
    #13

---
+++ PhotoTag/PhotoTag/Photo Note/PhotoNoteViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoNoteViewController.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2020/11/06.
+//
+
+import UIKit
+
+class PhotoNoteViewController: UIViewController {
+
+    weak var coordinator: PhotoNoteCoordinator?
+    
+    //TODO:- add viewModel as parameter
+    init(coordinator: PhotoNoteCoordinator) {
+        self.coordinator = coordinator
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) { return nil }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
