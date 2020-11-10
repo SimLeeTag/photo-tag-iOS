9dbe7c2d0214a53d3b29f9a0bbf4868088bb18a6
dev-Lena <keunnalee@gmail.com>
2020-11-11 00:10:36 +0900

    feat: divide common contentView into two classes
    - ScrollableContentViewWithHead
    - ContentViewWithHead
    
    #14, #15

---
+++ PhotoTag/PhotoTag/Common View/ContentViewWithHead.swift
@@ -0,0 +1,48 @@
+//
+//  ContentViewWithHead.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2020/11/11.
+//
+
+import UIKit
+
+protocol HeaderRepresentable: Representable {
+    var headerStackView: UIStackView { get set }
+    var spaceView: UIView { get set }
+    
+    func configureHeaderView()
+}
+
+class ContentViewWithHead: ContentView, HeaderRepresentable {
+    var headerStackView = SubviewFactory.headerStackView()
+    var spaceView = SubviewFactory.spaceView()
+    
+    func configureHeaderView() { }
+    
+    override func configureContentView() {
+        contentView.addSubview(contentStackView)
+    }
+}
+
+private extension ContentViewWithHead {
+    struct SubviewFactory {
+        
+        static func headerStackView() -> UIStackView {
+            let stackView = UIStackView()
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            stackView.alignment = .fill
+            stackView.axis = .horizontal
+            stackView.distribution = .fill
+            return stackView
+        }
+        
+        static func spaceView() -> UIView {
+            let view = UIView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.backgroundColor = .clear
+            return view
+        }
+    }
+}
+
--- PhotoTag/PhotoTag/Common View/ScrollableContentViewWithHead.swift
+++ PhotoTag/PhotoTag/Common View/ScrollableContentViewWithHead.swift
@@ -13,50 +13,23 @@ protocol Scrollable: Representable {
     func configureScrollView()
 }
 
-protocol HeaderRepresentable: Representable {
-    var headerStackView: UIStackView { get set }
-    var spaceView: UIView { get set }
+class ScrollableContentViewWithHead: ContentViewWithHead, Scrollable {
     
-    func configureHeaderView()
-}
-
-class ScrollableContentViewWithHead: ContentView, HeaderRepresentable, Scrollable {
-    
-    var headerStackView = SubviewFactory.headerStackView()
     var scrollView = SubviewFactory.sceneScrollView()
-    var spaceView = SubviewFactory.spaceView()
-    
-    func configureHeaderView() { }
-    
-    func configureScrollView() {
-        self.addSubview(contentView)
-        scrollView.addSubview(contentView)
-    }
     
     override func configureContentView() {
         contentView.addSubview(contentStackView)
         configureScrollView()
     }
+    
+    func configureScrollView() {
+        scrollView.addSubview(contentView)
+    }
+    
 }
 
 private extension ScrollableContentViewWithHead {
     struct SubviewFactory {
-        
-        static func headerStackView() -> UIStackView {
-            let stackView = UIStackView()
-            stackView.translatesAutoresizingMaskIntoConstraints = false
-            stackView.alignment = .fill
-            stackView.axis = .horizontal
-            stackView.distribution = .fillProportionally
-            return stackView
-        }
-        
-        static func spaceView() -> UIView {
-            let view = UIView()
-            view.translatesAutoresizingMaskIntoConstraints = false
-            view.backgroundColor = .clear
-            return view
-        }
         
         static func sceneScrollView() -> UIScrollView {
             let scrollView = UIScrollView()
