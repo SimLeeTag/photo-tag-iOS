3b8a632710c6422f6687685bbda3b84a10fc7312
dev-Lena <keunnalee@gmail.com>
2020-11-12 14:16:56 +0900

    feat: add Tag Category Collection View Cell class and xib file
    #14

--- PhotoTag/PhotoTag/Tag/TagCategory/View/TagCategoryCollectionViewCell.swift
+++ PhotoTag/PhotoTag/Tag/TagCategory/View/TagCategoryCollectionViewCell.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class TagCategoryCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var latestImageView: UIImageView!
+    @IBOutlet weak var tagNameLabel: UILabel!
+    @IBOutlet weak var noteCountLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
