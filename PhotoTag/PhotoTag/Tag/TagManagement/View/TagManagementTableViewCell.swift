e67778e1790b032392bac8bc03a4273ee5580a4b
dev-Lena <keunnalee@gmail.com>
2020-11-12 14:17:31 +0900

    feat: add Tag Management Table View Cell class and xib file
    #15

--- PhotoTag/PhotoTag/Tag/TagManagement/View/TagManagementTableViewCell.swift
+++ PhotoTag/PhotoTag/Tag/TagManagement/View/TagManagementTableViewCell.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class TagManagementTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var tagNameLabel: UILabel!
+    @IBOutlet weak var noteCountLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
