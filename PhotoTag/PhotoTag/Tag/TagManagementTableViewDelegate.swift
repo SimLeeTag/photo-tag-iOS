c4c07a8197a5b390e20169add470b848df7f7a2e
dev-Lena <keunnalee@gmail.com>
2020-11-12 12:40:16 +0900

    feat: create TagManagementTableViewDelegate class for swipe menu
    #15, #19

---
+++ PhotoTag/PhotoTag/Tag/TagManagementTableViewDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  TagManagementTableViewDelegate.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2020/11/11.
+//
+
+import UIKit
+
+class TagManagementTableViewDelegate: NSObject, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        
+        if indexPath.section == TagManagementTableViewConstant.activeHashtagsSectionNumber {
+            return UISwipeActionsConfiguration(actions: [
+                makeArchiveContextualAction(tableView, forRowAt: indexPath)
+            ])
+        }
+        
+        return UISwipeActionsConfiguration(actions: [
+            makeRestoreContextualAction(tableView, forRowAt: indexPath)
+        ])
+    }
+    
+    private func makeArchiveContextualAction(_ tableView: UITableView, forRowAt indexPath: IndexPath) -> UIContextualAction {
+        return UIContextualAction(style: .destructive, title: "Archive") { (_, _, completion) in
+            tableView.moveSection(indexPath.section, toSection: TagManagementTableViewConstant.archivedHashtagsSectionNumber)
+            completion(true)
+        }
+    }
+    
+    private func makeRestoreContextualAction(_ tableView: UITableView, forRowAt indexPath: IndexPath) -> UIContextualAction {
+        return UIContextualAction(style: .normal, title: "Restore") { (_, _, completion) in
+            
+            tableView.moveSection(indexPath.section, toSection: TagManagementTableViewConstant.activeHashtagsSectionNumber)
+            completion(true)
+        }
+    }
+}
