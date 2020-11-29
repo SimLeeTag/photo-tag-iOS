fddf31378704460f1e2b7cefb58c50fe17b67064
dev-Lena <keunnalee@gmail.com>
2020-11-29 14:54:54 +0900

    feat: implement Observable class to using View Model
    #22

---
+++ PhotoTag/PhotoTag/Utility/Observable.swift
@@ -0,0 +1,27 @@
+//
+//  Observable.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2020/11/29.
+//
+
+import Foundation
+
+final class Observable<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
