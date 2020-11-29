b03f9fe8c68b0367a49cb8707b49ab60d2b66355
dev-Lena <keunnalee@gmail.com>
2020-11-29 15:38:35 +0900

    feat: create tag network management
    - add use case: fetch hashtags
    
    #22

---
+++ PhotoTag/PhotoTag/Tag/Model/TagManager.swift
@@ -0,0 +1,25 @@
+//
+//  TagManager.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2020/11/25.
+//
+
+import Foundation
+import Combine
+
+class TagNetworkManager {
+    func fetchHashtags(completionHandler: @escaping (Hashtags?) -> Void) {
+        
+        UseCase.shared
+            .request(type: Hashtags.self, endpoint: Endpoint(path: .fetchHashtags), method: .get)
+            .receive(subscriber: Subscribers.Sink(receiveCompletion: { [weak self] in
+                guard case let .failure(error) = $0 else { return }
+                print(#function)
+                debugPrint(error.localizedDescription)
+                // TODO: - present alertController
+            }, receiveValue: { [weak self] data in
+                completionHandler(data)
+            }))
+    }
+}
