2de8ced19c0f291aecce2c7c759e5e6f1627c0cf
Lena <keunnalee@gmail.com>
2020-12-23 01:36:12 +0900

    feat: add PhotoNotes json structure

---
+++ PhotoTag/PhotoTag/Tag/Model/PhotoNotes.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoNotes.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2020/12/17.
+//
+
+import Foundation
+
+struct PhotoNotes: Codable {
+    let notes: [Note]
+}
+
+struct Note: Codable {
+    let tagID, noteID: Int
+    let tags, photos: [String]
+    let created: Date
+    let rawMemo: String
+    
+    enum CodingKeys: String, CodingKey {
+        case tags, photos, created, rawMemo
+        case tagID = "tagId"
+        case noteID = "noteId"
+    }
+}
