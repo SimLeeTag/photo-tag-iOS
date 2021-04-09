68a0755f53761b4ec1bc9c9d813657552bc5a5c8
dev-Lena <keunnalee@gmail.com>
2021-04-09 16:53:05 +0900

    feat: make custom image view class named PhotoImageView
    - active activity indicator when it is loading
    - check cached image first before download image
    - if there is no cached image, request and download image and show
    
    #56

---
+++ PhotoTag/PhotoTag/Common View/PhotoImageView.swift
@@ -0,0 +1,61 @@
+//
+//  PhotoImageView.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2021/04/09.
+//
+
+import UIKit
+
+var imageCache = [String: UIImage]()
+
+final class PhotoImageView: UIImageView {
+    
+    // MARK: - Properties
+    // set activity indicator
+    var isLoading: Bool {
+      get { return activityIndicator.isAnimating }
+      set {
+        if newValue {
+          activityIndicator.startAnimating()
+        } else {
+          activityIndicator.stopAnimating()
+        }
+      }
+    }
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.hidesWhenStopped = true
+        indicator.startAnimating()
+        self.addSubview(indicator)
+        indicator.pinCenter(to: self)
+        return indicator
+    }()
+    var lastImgUrl: String?
+    
+    // MARK: - Functions
+    func loadImage(with urlString: String) {
+        
+        self.image = nil
+        lastImgUrl = urlString
+        
+        // Check cached image first
+        if let cachedImage = imageCache[urlString] {
+            self.image = cachedImage
+            self.isLoading = false
+            return
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        ImageDownloadManager.fetchImage(with: url) { [weak self] photoImage in
+            imageCache[url.absoluteString] = photoImage
+            DispatchQueue.main.async {
+                self?.image = photoImage
+                self?.isLoading = false
+            }
+        }
+    }
+    
+}
