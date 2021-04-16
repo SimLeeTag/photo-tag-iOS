3fb5c90fea00cf06dbb208e5d97ec7bd7fb374e8
dev-Lena <keunnalee@gmail.com>
2021-04-16 23:57:24 +0900

    feat: add method in UIImage extension for gradient effect
    #56

---
+++ PhotoTag/PhotoTag/Common View/BottomGradient.swift
@@ -0,0 +1,22 @@
+//
+//  BottomGradient.swift
+//  PhotoTag
+//
+//  Created by Keunna Lee on 2021/04/15.
+//
+
+import UIKit
+
+extension UIImage {
+    func gradientImageWithBounds(bounds: CGRect, colors: [CGColor]) -> UIImage {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors
+        
+        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
